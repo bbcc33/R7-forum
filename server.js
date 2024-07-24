589e237a85a775221f26a9349b48320d943e644c
bbcc33 <biancanboykin@gmail.com>
2024-07-24 13:50:21 -0600

    lesson6

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3001, () => {
+  console.log('Socket.IO server running at http://localhost:3001/');
+});
